dd64b30c08b63c10ae166b1c6cff29958f1f8381
Harshit Anand <harshitanand94@gmail.com>
2025-01-13 06:45:54 +0530

    update CORS configuration to allow requests from any origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS with specific configurations
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL(s)
+    origin: '*', // Replace with your frontend's URL(s)
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // Allow cookies and authorization headers
   });
